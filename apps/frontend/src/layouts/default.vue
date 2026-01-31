97b08d941dd50c9427d86cdbc34b9d7bbb2d4f27
Mark Wiemer <markwiemer@outlook.com>
2026-01-30 17:44:55 -0800

    chore: format default.vue

--- apps/frontend/src/layouts/default.vue
+++ apps/frontend/src/layouts/default.vue
@@ -523,7 +523,10 @@
       </div>
       <div class="links links-2" role="region" aria-label="Resources">
         <h4 aria-hidden="true">{{ formatMessage(footerMessages.resourcesTitle) }}</h4>
-        <a :target="$external()" href="https://github.com/mark-wiemer/nerds-minecraft-launcher/issues/new">
+        <a
+          :target="$external()"
+          href="https://github.com/mark-wiemer/nerds-minecraft-launcher/issues/new"
+        >
           {{ formatMessage(footerMessages.support) }}
         </a>
         <a :target="$external()" href="https://blog.modrinth.com">
