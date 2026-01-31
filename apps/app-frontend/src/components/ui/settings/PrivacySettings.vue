b700fc69f6acfc1f9d57e006515d141ce2bb594e
Mark Wiemer <markwiemer@outlook.com>
2026-01-30 20:27:13 -0800

    fix: remove reference to ads

--- apps/app-frontend/src/components/ui/settings/PrivacySettings.vue
+++ apps/app-frontend/src/components/ui/settings/PrivacySettings.vue
@@ -22,26 +22,6 @@ watch(
 </script>
 
 <template>
-  <div class="flex items-center justify-between gap-4">
-    <div>
-      <h2 class="m-0 text-lg font-extrabold text-contrast">Personalized ads</h2>
-      <p class="m-0 text-sm">
-        Modrinth's ad provider, Aditude, shows ads based on your preferences. By disabling this
-        option, you opt out and ads will no longer be shown based on your interests.
-      </p>
-    </div>
-    <Toggle
-      id="personalized-ads"
-      :model-value="settings.personalized_ads"
-      :checked="settings.personalized_ads"
-      @update:model-value="
-        (e) => {
-          settings.personalized_ads = e
-        }
-      "
-    />
-  </div>
-
   <div class="mt-4 flex items-center justify-between gap-4">
     <div>
       <h2 class="m-0 text-lg font-extrabold text-contrast">Telemetry</h2>
